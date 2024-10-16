721b948366b71f9905d893f0aabef4c82d03bd67
CS++ <kevin@csplusplus.com>
2024-10-16 11:57:51 -0700

    Update Unit9Test.java

--- src/test/java/com/csplusplus/Unit9Test.java
+++ src/test/java/com/csplusplus/Unit9Test.java
@@ -1,7 +1,7 @@
 package com.csplusplus;
 
-import static org.junit.Assert.*;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 public class Unit9Test {
     
